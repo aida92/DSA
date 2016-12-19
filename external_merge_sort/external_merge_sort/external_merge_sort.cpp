050a1c9b6e9ebccebb429a2eca3b178543b7b63d
aida <aidazolic@hotmail.com>
2016-12-19 09:17:19 +0100

    Initial

---
+++ external_merge_sort/external_merge_sort/external_merge_sort.cpp
@@ -0,0 +1,12 @@
+// external_merge_sort.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Salut!" << std::endl;
+    return 0;
+}
+
